8b00bccbbfcb3862a2c69249645c765f6876ad42
LuisCarlosJp <jardimpereiralc@gmail.com>
2023-06-18 17:54:21 -0300

    Funcionando com os seguintes 049-15,52-15,68-8

--- TumorAVC_v1.py
+++ TumorAVC_v1.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
+#Read file
 file_path = "archive(1)/computed-tomography-images-for-intracranial-hemorrhage-detection-and-segmentation-1.0.0/Patients_CT/049/brain/15.jpg"
 image = cv2.imread(file_path)
 
@@ -14,14 +15,21 @@ thresh=cv2.threshold(blurImage, 250,255, cv2.THRESH_BINARY)[1] #threshold the im
 dilate=cv2.dilate(thresh, None, iterations=10) #add pixels to the boundaries of the brain (not the inverse of the erosion - produced different image)
 erode=cv2.erode(dilate, None, iterations=6) #remove pixels of the boundaries of the brain
 
+#Find second largest contour
 contours, hierarchy = cv2.findContours(erode, cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
-mask= np.zeros_like(original, np.uint8)
-cv2.drawContours(mask, contours, 3, (255,255,255), -1)
-result = cv2.bitwise_and(original, original, mask=mask)
+sorted_contours= sorted(contours, key=cv2.contourArea, reverse= True)[1]
+
+
+#Draw mask
+aux = np.zeros_like(original, np.uint8)
+cv2.drawContours(aux, [sorted_contours], -1, (255,255,255), -1)
+cv2.imshow('Mascara', aux)
+result = cv2.bitwise_and(original, original, mask=aux)
 cv2.imshow('original', result)
 
-
 thresh = cv2.threshold(result,150,255,cv2.THRESH_BINARY_INV)[1]
+
+#identification tumor
 params = cv2.SimpleBlobDetector_Params()
 
 # Set the threshold
@@ -29,7 +37,8 @@ params.minThreshold = 10
 
 # Set the area filter
 params.filterByArea = True
-params.minArea = 500
+params.minArea = 447
+params.maxArea = 1450
 
 # Set the circularity filter
 params.filterByCircularity = True
